d994cff4eadee66112f322b027d57dcbc500ecec
ZHT <z1099511627776@outlook.com>
2026-03-23 18:42:10 +0800

    头文件

---
+++ threadpool.h
@@ -0,0 +1,18 @@
+#ifndef THREADPOOL_H
+#define THREADPOOL_H
+
+#include <pthread.h>
+
+typedef struct Task {
+    void *(*function)(void *arg);
+    void *arg;
+    void (*callback)(void *result);
+} Task;
+
+typedef struct Threadpool Threadpool;
+
+struct Threadpool* threadpool_create(int thread_count);
+int threadpool_add(struct Threadpool *pool, struct Task *task);
+void threadpool_destroy(struct Threadpool *pool);
+
+#endif
